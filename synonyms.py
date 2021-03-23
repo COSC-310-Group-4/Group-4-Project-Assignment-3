b526efec827059fe55e660f3b7733f858a1d4d9c
Monica Rampaul <monica.rampaul@alumni.ubc.ca>
2021-03-22 21:11:41 -0400

    one synonym option deleted

--- synonyms.py
+++ synonyms.py
@@ -1,15 +1,34 @@
+# Installations:
+# pip install ntlk (if not already installed)
+
 import nltk
 from nltk.corpus import wordnet
 
-# Gets the synonyms for the specified word and returns the set.
-# Also cleans the elements in the array (i.e., "-" or "_")
+# takes the sentance the user inputs as well as a list of entities from NER 
+# and returns an array of all words in the user input that does not include words in
+# the list of entities. 
+def getArray(input, entities):
+    if (" " in input):
+        arr = input.split(" ")
+        for i in entities:
+            for j in arr:
+                if i == j:
+                    arr.remove(j)
+        return arr
+    else:
+        return input
+
+# gets all the synonyms for a specified word and returns an array of those synonyms
 def getSyn(word):
     synonyms = []
 
+    # for each element in the synonym set for the specified word, the "name" attribute
+    # is appended to the synonyms array
     for i in wordnet.synsets(word):
         for j in i.lemmas():
             synonyms.append(j.name())
 
+    # removes special characters that wordnet uses for synonyms that are 2 words
     for i in synonyms:
         if "_" in i:
             new = i.replace("_", " ")
@@ -19,72 +38,73 @@ def getSyn(word):
             new = i.replace("-", " ")
             synonyms.append(new)
             synonyms.remove(i)
+    
+    # returns the synonyms array
     return synonyms
 
-# initializes all the arrays with their synonyms 
-movieSyn = getSyn("movie")
-directorSyn = getSyn("director")
-summarySyn = getSyn("summary")
-charactersSyn = getSyn("characters")
-playedSyn = getSyn("played")
-longSyn = getSyn("long")
-changeSyn = getSyn("change")
-nameSyn = getSyn("name")
-nevermindSyn = getSyn("disregard")
-workedSyn = getSyn("worked")
-otherSyn = getSyn("other")
-birthdaySyn = getSyn("birthday")
-placeSyn = getSyn("place")
-latestSyn = getSyn("latest")
-biographySyn = getSyn("biography")
-companySyn = getSyn("company")
-producedSyn = getSyn("produced")
-goodbyeSyn = getSyn("goodbye")
+# if the keyword specified to call each function is a synonym for the input the user
+# entered, the function returns 0 (it has been found) else, it return 1 (not found)
+def findSyns(arr, w):
+    s = []
+    found = 1
     
-# takes in the user input and an array (one of the ones initialized previousny)
-# compares them and returns 0 or 1 which will indicate which function should be called
-def getLocation(input, arr):
-    for i in arr:
-        if input.__contains__(i):
-            return 0
-    return 1
+    # if w is in arr, return 0
+    # else find the synonyms for each word in arr and if the synonyms contain w, return 0
+    if (arr.__contains__(w)):
+        found = 0
+    else:
+        for i in range(len(arr)):
+            word = arr[i]
+            s = getSyn(word)
+            if (s.__contains__(w)):
+                found = 0
+                break
+    
+    return found
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# the code below is used for testing purposes while implementing
-# this would be implemented in IMDBot (replacing what is currently there)
-input = "lets talk about a film"
-userName = "Monica"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Used for testing purposes:
+input = "Zendaya and Tom in movie"
+entities = ['Zendaya', "Tom"]
 
-if (getLocation(input, movieSyn) == 0):
-    #call findMovie(userName)
+a = getArray(input, entities)
+print(a)
+
+res = findSyns(a, "movie")
+#print(res)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# the below code is how synonyms would be implemented into IMDBot:
+
+user_input = getArray(input.lower(), entities)
+
+if (findSyns(user_input, "movie") == 0):
+    # call findMove(user_Name)
+    print("Ok, let's talk about a movie")
+elif(findSyns(user_input, "director") == 0):
+    # check if movie in locals(), 
+    # call findDirector(movie)
+    # if movie not in locals(), display error message
     pass
-elif (getLocation(input, directorSyn) == 0):
-    #check if movie is in locals,
-    #call findDirector(movie) if movie is in locals
-    #else print error message
+elif (findSyns(user_input, "summary") == 0):
+    # check if movie in locals(), 
+    # call giveSummary(movie)
+    # if movie not in locals(), display error message
     pass
-elif (getLocation(input, summarySyn) == 0):
-    #check if movie is in locals,
-    #call giveSummary(movie) if movie is in locals
-    #else print error message
+elif (findSyns(user_input, "characters") == 0):
+    # check if movie in locals(), 
+    # call showCharacters(movie)
+    # if movie not in locals(), display error message
     pass
-elif (getLocation(input, charactersSyn) == 0):
-    #check if movie is in locals,
-    #call showCharacters(movie) if movie is in locals
-    #else print error message
-    pass
-elif (getLocation(input, playedSyn) == 0):
-    #check if movie is in locals,
-    #splits string where necessary to get character name
-    #call whoPlayed(movie, character) if movie is in locals
-    #else print error message
-    pass
-#continues exactly like this until all the synonym arrays have been checked
+# ... etc ...
 else:
-    #print error message
+    #display error msg
+    print("Sorry, i didn't quite get that")
     pass
 
-# limitation:
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Limitations:
 #   - synonyms are limited by the synonyms in wordnet
 #   - slang cannot be used
-#   - can't handle an input having the same synonym for multiple keywords
+#   - some keywords have the same synonyms (i.e, "role" is a synonym for "character" and "worked")
+#       this can be conflicting depending on what the user wants to ask the chatbot
