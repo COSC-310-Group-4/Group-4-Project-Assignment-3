42d701284c730d2b0728e5169cfb98ffea8ed717
Orvin Tritama <orvinrfc@hotmail.com>
2021-03-24 18:00:10 -0700

    gui.py still opens a window. sub-menu is still working

--- gui.py
+++ gui.py
@@ -1,66 +1,98 @@
 from tkinter import *
+import tkinter
+import film as f
+import person as p
+import company as c
+import user as u
+import webbrowser
 
-# Command for Menu File > Exit
-def exitProgram():
-    exit()
+class IMDBotGUI():
 
-# ButtonClick listener
-def buttonClick():
-    userInput = inputBox.get("1.0", "end-1c") # Take the inputBox text from 1st word, 0th character, to the end without the new line
-    print(userInput)
+    def __init__(self,master):
+        self.master = master
+        # Initialization
+        master.title("IMDBot")
+        master.geometry("500x500")
+        master.minsize(300, 250) # minimum size so that the button and text boxes don't disappear or look odd
+
+        # Menu Bar
+        mainMenu = Menu(master)
+
+
+        # Open Git's IMDBot Readme
+        new = 1
+        url = "https://github.com/COSC-310-Group-4/Group-4-Project-Assignment-3/blob/main/README.md"
+
+        def openReadme():
+            webbrowser.open(url,new=new)
+
+        # Sub Menu for "File"
+        self.fileMenu = Menu(mainMenu, tearoff=0, background="#555555", foreground="white", activebackground="black", activeforeground="white")
+        self.fileMenu.add_command(label="New Conversation")
+        self.fileMenu.add_command(label="Exit", command=self.exitProgram)
+
+        # Sub Menu For "Help"
+        self.helpMenu = Menu(mainMenu, tearoff=0, background="#555555", foreground="white", activebackground="black", activeforeground="white")
+        self.helpMenu.add_command(label="About IMDBot", command=openReadme)
+
+        # Main Menu
+        mainMenu.add_cascade(label="File", menu=self.fileMenu)
+        mainMenu.add_cascade(label="Help", menu=self.helpMenu)
+
+        # Add the menus to the program  
+        master.config(menu=mainMenu)
+
+        # Create the text area of conversation
+        self.outputBox = Text(master, state = "disabled", bg = "#555555",height = 10, fg = "white", font = ("Trebuchet 12"))
+        self.outputBox.pack(side = TOP, fill="both", expand = True, padx = 5, pady = 5)   
+
+        # Create the text widget for user input
+        #self.inputBox = Text(master, state = "normal", bg = "#999999", height = 2, width = 10, fg = "black", font = ("Trebuchet 12"))
+        self.input_var=StringVar()
+        self.inputBox = Entry(master,textvariable = self.input_var, font=('calibre',10,'normal'))
+        self.inputBox.pack(side = LEFT, fill = 'both', expand = True, padx = 5, pady = 5)
+
+        # Create the send button
+        self.send_button = Button(master,text = "Send",width = 10, height = 2,bg = "#555555", activebackground = "#3B3B3B",fg = "white", command=lambda: self.buttonClick())
+        self.send_button.pack(side = RIGHT, padx = 5, pady = 5, expand=0)
+        
+
+        # Create the scroll bar
+        self.scrollbar = Scrollbar(self.outputBox, orient = VERTICAL) # create the scrollbar
+        self.scrollbar.pack(side = RIGHT, fill = Y) # put it on the right side filling vertically 
+        self.outputBox['yscrollcommand'] = self.scrollbar.set # pack it inside the outputBox
+
+        # will change after user click the button
+        self.userInput=""
+
+
+    # Command for Menu File > Exit
+    def exitProgram(self):
+        exit()
+
+    # ButtonClick listener
+    def buttonClick(self):
+        self.userInput = self.inputBox.get() # Take the inputBox text from 1st word, 0th character, to the end without the new line
+        # self.userInput = self.input_var.get()
+
+        # Write to the outputBox, don't forget to disable it after
+        self.outputBox["state"] = "normal"
+        self.outputBox.insert("end-1c", self.userInput + "\n") # insert to the very last and create \n. so that next message will pop at the very end and automatically adds a new line
+        self.outputBox["state"] = "disabled" 
+        # Clear the input box
+        self.inputBox.delete(0, tkinter.END)
+        return self.userInput
+
+    # helper function to add IMDBot's text to the GUI
+    def addToOutputBox(self,text):
+        # Write to the outputBox, don't forget to disable it after
+        self.outputBox["state"] = "normal"
+        self.outputBox.insert("end-1c", text + "\n") # insert to the very last and create \n. so that next message will pop at the very end and automatically adds a new line
+        self.outputBox["state"] = "disabled" 
+
+    def getUserInput(self):
+        return self.userInput
     
-    # Write to the outputBox, don't forget to disabled it after
-    outputBox["state"] = "normal"
-    outputBox.insert("end-1c", userInput + "\n") # insert to the very last and create \n. so that next message will pop at the very end and automatically adds a new line
-    outputBox["state"] = "disabled" 
-
-    # Clear the input box
-    inputBox.delete("1.0", "end-1c")
-
-# Initialization
-root = Tk()
-root.title("IMDBot")
-root.geometry("500x500")
-root.minsize(300, 250) # minimum size so that the button and text boxes don't disappear or look odd
-
-# Menu Bar
-mainMenu = Menu(root)
-
-# Sub Menu for "File"
-fileMenu = Menu(mainMenu, tearoff=0, background="#555555", foreground="white", activebackground="black", activeforeground="white")
-fileMenu.add_command(label="New Conversation")
-fileMenu.add_command(label="Save")
-fileMenu.add_command(label="Save As...")
-fileMenu.add_command(label="Exit", command=exitProgram)
-
-# Sub Menu For "Help"
-helpMenu = Menu(mainMenu, tearoff=0, background="#555555", foreground="white", activebackground="black", activeforeground="white")
-helpMenu.add_command(label="View Help")
-helpMenu.add_command(label="About IMDBot")
-
-# Main Menu
-mainMenu.add_cascade(label="File", menu=fileMenu)
-mainMenu.add_cascade(label="Help", menu=helpMenu)
-
-# Add the menus to the program  
-root.config(menu=mainMenu)
-
-# Create the text area of conversation
-outputBox = Text(root, state = "disabled", bg = "#555555",height = 10, fg = "white", font = ("Trebuchet 12"))
-outputBox.pack(side = TOP, fill="both", expand = True, padx = 5, pady = 5)   
-
-# Create the text widget for user input
-inputBox = Text(root, state = "normal", bg = "#999999", height = 2, width = 10, fg = "black", font = ("Trebuchet 12"))
-inputBox.pack(side = LEFT, fill = 'both', expand = True, padx = 5, pady = 5)
-
-# Create the send button
-send_button = Button(root,text = "Send",width = 10, height = 2,bg = "#555555", activebackground = "#3B3B3B",fg = "white", command=lambda: buttonClick())
-send_button.pack(side = RIGHT, padx = 5, pady = 5, expand=0)
-
-# Create the scroll bar
-scrollbar = Scrollbar(outputBox, orient = VERTICAL) # create the scrollbar
-scrollbar.pack(side = RIGHT, fill = Y) # put it on the right side filling vertically 
-outputBox['yscrollcommand'] = scrollbar.set # pack it inside the outputBox
-
-# Run
+root = Tk() 
+imdbotgui = IMDBotGUI(root)
 root.mainloop()
